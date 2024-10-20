90e51d72d745561d4d567322218d76ec862447e7
solar0037 <solar0037@gmail.com>
2024-10-20 20:54:38 +0900

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config("나는 배웠다")
+    st.title("나는 배웠다")
+    st.write("지금까지 배운 수학, 과학, 공학 분야의 지식을 정리하는 프로젝트입니다.")
+    st.write("사용 기술: Python, Streamlit, Schemdraw 등")
+    st.link_button("GitHub", "https://github.com/solar0037/whatilearned")
+
+
+if __name__ == '__main__':
+    main()
